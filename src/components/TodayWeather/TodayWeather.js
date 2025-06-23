54de017eaa951292d413ce179278560eda60970e
AxeMello <akselcruses@gmail.com>
2025-06-23 13:59:47 +0530

    On branch main Initial commit 	new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   public/screenshot.png 	new file:   src/App.js 	new file:   src/api/OpenWeatherService.js 	new file:   src/assets/humidity.svg 	new file:   src/assets/icons/01d.png 	new file:   src/assets/icons/01n.png 	new file:   src/assets/icons/02d.png 	new file:   src/assets/icons/02n.png 	new file:   src/assets/icons/03d.png 	new file:   src/assets/icons/03n.png 	new file:   src/assets/icons/04d.png 	new file:   src/assets/icons/04n.png 	new file:   src/assets/icons/09d.png 	new file:   src/assets/icons/09n.png 	new file:   src/assets/icons/10d.png 	new file:   src/assets/icons/10n.png 	new file:   src/assets/icons/11d.png 	new file:   src/assets/icons/11n.png 	new file:   src/assets/icons/13d.png 	new file:   src/assets/icons/13n.png 	new file:   src/assets/icons/50d.png 	new file:   src/assets/icons/50n.png 	new file:   src/assets/icons/unknown.png 	new file:   src/assets/logo.png 	new file:   src/assets/splash-icon.svg 	new file:   src/components/Reusable/ErrorBox.js 	new file:   src/components/Reusable/Layout.js 	new file:   src/components/Reusable/LoadingBox.js 	new file:   src/components/Reusable/SectionHeader.js 	new file:   src/components/Reusable/UTCDatetime.js 	new file:   src/components/Search/Search.js 	new file:   src/components/TodayWeather/AirConditions/AirConditions.js 	new file:   src/components/TodayWeather/AirConditions/AirConditionsItem.js 	new file:   src/components/TodayWeather/Details/CityDateDetail.js 	new file:   src/components/TodayWeather/Details/Details.js 	new file:   src/components/TodayWeather/Details/TemperatureWeatherDetail.js 	new file:   src/components/TodayWeather/Details/WeatherIconDetail.js 	new file:   src/components/TodayWeather/Forecast/DailyForecast.js 	new file:   src/components/TodayWeather/Forecast/DailyForecastItem.js 	new file:   src/components/TodayWeather/TodayWeather.js 	new file:   src/components/WeeklyForecast/DayWeatherDetails.js 	new file:   src/components/WeeklyForecast/UnfedForecastItem.js 	new file:   src/components/WeeklyForecast/WeeklyForecast.js 	new file:   src/components/WeeklyForecast/WeeklyForecastItem.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/utilities/ApiService.js 	new file:   src/utilities/DataUtils.js 	new file:   src/utilities/DateConstants.js 	new file:   src/utilities/DatetimeUtils.js 	new file:   src/utilities/IconsUtils.js

---
+++ src/components/TodayWeather/TodayWeather.js
@@ -0,0 +1,15 @@
+import { Grid } from '@mui/material';
+import React from 'react';
+import AirConditions from './AirConditions/AirConditions';
+import Details from './Details/Details';
+
+const TodayWeather = ({ data, forecastList }) => {
+  return (
+    <Grid container sx={{ padding: '3rem 0rem 0rem' }}>
+      <Details data={data} />
+      <AirConditions data={data} />
+    </Grid>
+  );
+};
+
+export default TodayWeather;
